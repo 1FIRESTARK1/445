328481c8a745bf630e9544e0b4696f82877812d5
Petr <stamovPetr@gmail.com>
2023-05-18 19:25:02 +0500

    321

---
+++ src/routes/blog/posts/2/+page.svelte
@@ -0,0 +1,5 @@
+<h1>My Second Blog Post</h1>
+
+'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
+
+'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
