7a2499c75d0060c5ba834d154c2530ee4831f0b3
Petr <stamovPetr@gmail.com>
2023-05-18 18:56:25 +0500

    2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,9 @@
       <a href="/" class="nav-logo" title="Back to Homepage">Пётр Стамов</a>
       <div class="nav-links">
        
-        {#each nav as link}
-<a href={link.href} class="link">{link.title}</a>
-{/each}
+        {#if link.isShow}
+        <a href={link.href} class="link">{link.title}</a>
+        {/if}        
 
       </div>
     </div>
@@ -16,29 +16,34 @@
     <slot></slot>
   </div>
   <script>
-    const nav = [
-    {
-      title: 'Обо мне',
-      href: '/about'
-    },
-    {
-      title: 'Блог',
-      href: '/blog'
-    },
-    {
-      title: 'Портфолио',
-      href: '/project'
-    },
-    {
-      title: 'Контакты',
-      href: '/contact'
-    },
-    {
-  "title": "temp",
-  "href": "/temp"
-}
+const nav = [
+  {
+    title: 'Обо мне',
+    href: '/about',
+    isShow: true,
+  },
+  {
+    title: 'Блог',
+    href: '/blog',
+    isShow: true,
+  },
+  {
+    title: 'Портфолио',
+    href: '/project',
+    isShow: true,
+  },
+  {
+    title: 'Контакты',
+    href: '/contact',
+    isShow: true,
+  },
+  {
+    title: 'temp',
+    href: '/temp',
+    isShow: false,
+  },
+];
 
-  ]
 </script>
 
   <style>
