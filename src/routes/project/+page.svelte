f2929de06fb89c79256a3f1ebf02912e1e1a1ce3
Petr <stamovPetr@gmail.com>
2023-05-23 19:25:29 +0500

    1

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,49 +1,47 @@
 <svelte:head>
-	<title>РџРѕСЂС‚С„РѕР»РёРѕ</title>
+ <title>Портфолио</title>
 </svelte:head>
 
-<div class='container'>
-	<h1>РњРѕРё РїСЂРѕРµРєС‚С‹</h1>
-	<div class='apps'>
-		{#each routes as page}
-			<div class='app'>
-				<h2>{page.title}</h2>
-				<p>{page.body}</p>
-				<p class='readmore'>
-					<a class='link' href={`/project/apps/${page.id}`}>
-						Р§РёС‚Р°С‚СЊ РґР°Р»РµРµ
-					</a>
-				</p>
-			</div>
-		{/each}
-	</div>
-</div>
-
 <script>
-	import { routes } from './routes.js';
-</script>
+    import { routes } from './routes.js';
+   </script>   
 
-
+<div class='container'>
+ <h1>Мои проекты</h1>
+ <div class='apps'>
+  {#each routes as page}
+   <div class='app'>
+    <h2>{page.title}</h2>
+    <p>{page.body}</p>
+    <p class='readmore'>
+     <a class='link' href={`/project/apps/${page.id}`}>
+      Читать далее
+     </a>
+    </p>
+   </div>
+  {/each}
+ </div>
+</div>
 <style>
     .container {
-      margin: 50px auto;
-      max-width: 800px;
-      padding: 0 20px;
+     margin: 50px auto;
+     max-width: 800px;
+     padding: 0 20px;
     }
   
     .apps {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      grid-gap: 20px;
+     display: grid;
+     grid-template-columns: 1fr 1fr;
+     grid-gap: 20px;
     }
-  
     .app {
-      border: 1px solid #ddd;
-      padding: 10px;
-      box-shadow: 0 0 10px #eee;
-    }
+   border: 1px solid #ddd;
+   padding: 10px;
+   box-shadow: 0 0 10px #eee;
+  }
+
+  .link {
+   color: rgb(10, 10, 139);
+  }
   
-    .link {
-      color: rgb(10, 10, 139);
-    }
-  </style>
+ </style>
