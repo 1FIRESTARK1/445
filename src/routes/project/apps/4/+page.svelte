2b62311fb0b05fbcf8fd3d651ca96bafd60c56a4
Petr <stamovPetr@gmail.com>
2023-05-30 16:55:39 +0500

    1

---
+++ src/routes/project/apps/4/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Сайт</h1>
+<p>
+</p>
+<button type="submit"><a href="https://classy-clafoutis-5890a5.netlify.app">перейти</button>
+<style>
+    button {
+  padding: 5px 10px;
+  background-color: #9e0f0f;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+ }
+</style>
